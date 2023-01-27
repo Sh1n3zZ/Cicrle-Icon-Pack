7c226985d7c44423f6ddb399e461c65ff7708f90
Sh1n3zZ <DengJunHai@foxmail.com>
2023-01-27 16:17:22 +0800

    适配更多图标 界面优化✨
    适配更多图标 界面优化✨

--- buildSrc/src/main/java/Blueprint.kt
+++ buildSrc/src/main/java/Blueprint.kt
@@ -2,6 +2,6 @@
 
 object Blueprint {
     const val appId = "com.chen.ChongYuan.app"
-    const val version = 230
-    const val versionName = "1.0.3"
+    const val version = 231
+    const val versionName = "1.0.4"
 }
